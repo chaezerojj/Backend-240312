06b439a2efb1715f4e8074edfc4baa04c6e83d6a
chaezerojj <yyzz1219@gmail.com>
2024-03-12 16:49:26 +0900

    240312

---
+++ PassionJava/src/chapter02/UseVariable3.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class UseVariable3 {
+
+	public static void main(String[] args) {
+		// Q. 정사각형의 둘레를 구하는 코드 작성 ( 둘레 = 한변 * 4)
+		int squareSide;
+		squareSide = 4;
+		System.out.println("정사각형의 둘레 = " + (squareSide * 4));
+		
+	}
+
+}
