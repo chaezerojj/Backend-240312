f68aaa2264b85813e1a9224034076d4be04282b0
chaezerojj <yyzz1219@gmail.com>
2024-03-25 16:50:10 +0900

    240325

---
+++ PassionJava/src/chapter21/Person.java
@@ -0,0 +1,21 @@
+package chapter21;
+
+public class Person {
+	private String name;
+
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ PassionJava/src/chapter21/Quiz1/BoardDao.java
@@ -0,0 +1,12 @@
+package chapter21.Quiz1;
+
+import java.util.*;
+
+public class BoardDao {
+	
+	public List<Board> getBoardList() {
+		
+		return null;
+	}
+	
+}
