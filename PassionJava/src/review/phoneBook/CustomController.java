a7a472f09c607b29bf447532a7a2de46aedb8fa1
chaezerojj <yyzz1219@gmail.com>
2024-04-04 16:45:58 +0900

    240404

---
+++ PassionJava/src/review/phoneBook/CustomController.java
@@ -0,0 +1,21 @@
+package review.phoneBook;
+
+public class CustomController extends PersonController{
+	
+	// 커스텀한 출력 만들기
+	// 상속을 통한 메소드 오버라이딩
+	@Override
+	public void getPersons() {
+		for (Person person: persons) {
+			System.out.println("***********************");
+			System.out.println("이름: ");
+			System.out.println(person.getName());
+			System.out.println("이름: ");
+			System.out.println(person.getPhone());
+			System.out.println("***********************");
+			
+		}
+	}
+	// 상속, 인터페이스, 람다
+	
+}
